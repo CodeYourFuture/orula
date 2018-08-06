7af97ce08805313258d6f9a1683c4d8b170612e1
Islam Dudaev <islamwolf23@gmail.com>
2018-08-06 21:48:49 +0100

    Use axios instread of fetch

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,7 +1,8 @@
+import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URI || "http://localhost:4000";
 
 export const getStatus = () => {
-  return fetch(`${API_URL}/api/status`).then(res => res.json());
+  return axios.get(`${API_URL}/api/status`).then(res => res.json());
 };
 
 export const getStudents = () => {
