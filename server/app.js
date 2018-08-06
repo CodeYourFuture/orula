b6bfc7ec066a3fb1e03d0b687d01b2fc004a9650
tiredsoldier <harunk3uk@gmail.com>
2018-08-06 13:11:40 +0100

    Add script to run client and server from top level

--- server/app.js
+++ server/app.js
@@ -10,5 +10,5 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/api/status", (req, res) => res.send({ status: "OK" }));
 
 app.listen(SERVER_PORT, () =>
-  console.log(`Orula server running on ${SERVER_PORT}`)
+  console.log(`Orula server running on http://localhost:${SERVER_PORT}`)
 );
