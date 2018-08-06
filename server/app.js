f1d7b0587bed0f020ee9e816b999a8655bd94ef8
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-08-06 16:20:32 +0100

    Use express.Router to organise the code

--- server/app.js
+++ server/app.js
@@ -2,13 +2,18 @@ const SERVER_PORT = process.env.PORT || 4000; // setup the server port number
 const express = require("express");
 const exphbs = require("express-handlebars");
 const cors = require("cors");
+const api = require("./api");
+const admin = require("./admin");
 
 const app = express();
 
-app.engine("hbs", exphbs({
-  defaultLayout: "main",
-  extname: "hbs"
-}));
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: "hbs"
+  })
+);
 
 app.set("view engine", "hbs");
 
@@ -18,11 +23,9 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/admin/courses", (req, res) => {
-  res.render("courses");
-});
+app.use("/admin", admin);
 
-app.get("/api/status", (req, res) => res.send({ status: "OK" }));
+app.use("/api", api);
 
 app.listen(SERVER_PORT, () =>
   console.log(`Orula server running on http://localhost:${SERVER_PORT}`)
