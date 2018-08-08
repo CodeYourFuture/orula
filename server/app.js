e7025b44745aed53faadec1381651bbb46550558
Islam Dudaev <islamwolf23@gmail.com>
2018-08-08 12:29:40 +0100

    Add SB Admin 2 theme to the server public folder

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const SERVER_PORT = process.env.PORT || 4000; // setup the server port number
 const express = require("express");
 const exphbs = require("express-handlebars");
+const path = require("path");
 const cors = require("cors");
 const api = require("./api");
 const admin = require("./admin");
@@ -14,10 +15,11 @@ app.engine(
     extname: "hbs"
   })
 );
-
 app.set("view engine", "hbs");
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -26,6 +28,8 @@ app.get("/", (req, res) => {
 app.use("/admin", admin);
 
 app.use("/api", api);
+
+
 
 app.listen(SERVER_PORT, () =>
   console.log(`Orula server running on http://localhost:${SERVER_PORT}`)
