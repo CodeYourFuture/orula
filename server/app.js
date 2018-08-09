f70d8c6a8193f02f44201c13d3395bc06b33b7a9
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-08-09 18:07:12 +0100

    Fix the confilect #34

--- server/app.js
+++ server/app.js
@@ -14,66 +14,12 @@ app.engine(
     extname: "hbs"
   })
 );
-<<<<<<< HEAD
 
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-    host: "127.0.0.1",
-    user: "cyf",
-    password: "password",
-    database: "orula",
-    port: 5432
-  }
-});
-
-
-// post '/classes/'
-// app.post("/classes", (req, res) =>{
-//   const body = req.body;
-//   knex('classes').insert([{
-//       class_id:
-//       `${ body.class_id }`,
-//       name: `${body.name}`,
-//       created_at: `${body.created_at}`,
-//       updated_at:`${
-//       body.updated_at}`
-//   }]).then(
-//             data => {
-//             res.send("successfully classes added");
-//       });
-// });
-
-// // put '/classes/:id'
-// app.put("/classes/:id",  (req, res) => {
-//   const class_id = `${req.params.id}`;
-//   const body = req.body;
-//   knex('classes').where('class_id', '=', class_id).update({
-//       name: `${ body.name }`,
-//       created_at: `${body.created_at}`
-//             }).then(data => {
-//                res.json(data);
-//            });
-// });
-
-// // delete '/classes/:id'
-// app.delete("/classes/:id", (req, res) =>{
-//   const class_id = `${req.params.id}`;
-//   knex(classes).where(
-//                 'class_id','=',class_id).del().then(
-//                 data => {
-//                  res.json(data);
-//                  });
-// });
-
-
-=======
->>>>>>> bda0f8b856039c3c89d65207874b48d750d57850
 app.set("view engine", "hbs");
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home", {
