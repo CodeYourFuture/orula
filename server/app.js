5006b58aed968fa78c2fb8eee694943fda9bcb39
Daniel Grant <danieljohngrant@gmail.com>
2018-08-18 12:43:06 +0100

    Refactor app to run static build from node server

--- server/app.js
+++ server/app.js
@@ -19,17 +19,19 @@ app.set("view engine", "hbs");
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.render("home", {
-    title: "Dashboard"
-  });
-});
-
 app.use("/admin", admin);
 
 app.use("/api", api);
+
+// In development environemnt, we use the create-react-app dev server
+// In production, the static build is served from here
+if (process.env.NODE_ENV !== "development") {
+  app.use("/", express.static(path.resolve(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
 
 app.listen(SERVER_PORT, () =>
   console.log(`Orula server running on http://localhost:${SERVER_PORT}`)
