26c450e39777825f07311a7943cea785fbd8a11b
Islam Dudaev <islamwolf23@gmail.com>
2018-08-22 14:07:42 +0100

    Add route for AddCourse in App.js

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 import Dashboard from "../Dashboard/Dashboard";
 import Courses from "../Courses/Courses";
 import AddOrganisation from "../Admin/AddOrganisation/AddOrganisation";
+import AddCourse from "../Courses/AddCourse/AddCourse";
 import Nav from "../../components/Nav/Nav";
 import axios from "axios";
 
@@ -38,6 +39,7 @@ class App extends Component {
               path="/admin/organisations/add"
               component={AddOrganisation}
             />
+            <Route path="/admin/courses/add" component={AddCourse} />
           </div>
         </div>
       </Router>
