e7a16253a880f05c0e6dcab571a9f76908cc524c
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-09-09 19:35:46 +0100

    Create editUser file

--- client/src/containers/User/EditUser/editUser.js
+++ client/src/containers/User/EditUser/editUser.js
@@ -1,2 +1,136 @@
 import React, { Component } from "react";
-import { getUsers, updateUser } from "../../../../helpers/api";
+import { getUserProfile, updateUserProfile } from "../../../helpers/api";
+import { Link } from "react-router-dom";
+
+class EditUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_id: "",
+      name: "",
+      message: "",
+      messageAlert: ""
+    };
+  }
+
+  componentDidMount = async () => {
+    const user_id = this.props.match.params.courseId;
+    const course = await getCourseById(course_id);
+    const { name, location, organisation_id } = course.data[0];
+    this.setState({
+      course_id,
+      name,
+      location,
+      organisation_id
+    });
+  };
+
+  handleOnchange = (input, e) => {
+    const value = e.target.value;
+    this.setState({ [input]: value });
+  };
+
+  // put it to /api/courses/:id
+  onSubmit = async e => {
+    e.preventDefault();
+    const { course_id, name, location, organisation_id } = this.state;
+    if (name === "" || location === "") {
+      this.setState({
+        message: "You must fill all the fields!",
+        messageAlert: "alert alert-danger"
+      });
+    } else {
+      try {
+        const res = await editCourse(
+          course_id,
+          name,
+          location,
+          organisation_id
+        );
+        this.setState({
+          message: res.data,
+          messageAlert: "alert alert-success"
+        });
+      } catch (err) {
+        this.setState({
+          message: err.response.data,
+          messageAlert: "alert alert-danger"
+        });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-lg-12">
+            <h2 className="page-header">Edit Course</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="panel panel-default">
+              <div className="panel-heading">Course details</div>
+              <div className="panel-body">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <form>
+                      <div className="form-group">
+                        <label className="control-label" htmlFor="name">
+                          Name
+                        </label>
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="name"
+                          id="name"
+                          onChange={e => this.handleOnchange("name", e)}
+                          value={this.state.name}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label className="control-label" htmlFor="location">
+                          Location
+                        </label>
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="location"
+                          id="location"
+                          onChange={e => this.handleOnchange("location", e)}
+                          value={this.state.location}
+                        />
+                      </div>
+                      <button
+                        type="submit"
+                        className="btn btn-primary"
+                        onClick={e => this.onSubmit(e)}
+                      >
+                        Save
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {this.state.message && (
+              <div className={this.state.messageAlert}>
+                {this.state.message}
+              </div>
+            )}
+          </div>
+          <div className="col-lg-12">
+            <Link to="/admin/courses">
+              <button className="btn btn-primary">
+                <i className="fa fa-eye fa-fw" /> View all courses
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default editUser;
