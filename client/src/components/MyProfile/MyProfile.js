42a6ce8ec3411f32b1e1409d3b0f04ffe0847f96
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-09-16 10:43:34 +0100

    Create edit button to myprofile

--- client/src/components/MyProfile/MyProfile.js
+++ client/src/components/MyProfile/MyProfile.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 class MyProfile extends Component {
   render() {
     return (
       <div className="row">
         <div className="col-lg-12">
           <h1 className="page-header">My profile</h1>
+          <Link to={`/user/profile/edit`}>
+            <button className="btn btn-success">
+              <i className="fa fa-edit fa-fw" /> Edit
+            </button>
+          </Link>
         </div>
       </div>
     );
