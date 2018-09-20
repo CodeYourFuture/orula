6e203e4e9ac1d22c7a5b6de16150d806a2987c75
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-09-20 11:30:12 +0100

    Move Dashboard folder to components folder

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Admin Dashboard</h2>
+        <div className="row">
+          <div className="col-lg-12">
+            <Link to="/admin/organisations">
+              <button className="btn btn-primary">
+                <i className="fa fa-eye fa-fw" /> View Organisations
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <Link to="/admin/organisations/add">
+              <button className="btn btn-primary">
+                <i className="fa fa-plus fa-fw" /> Add Organisation
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <Link to="/admin/lessons">
+              <button className="btn btn-primary">
+                <i className="fa fa-eye fa-fw" /> View Lessons
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <Link to="/admin/lessons/add">
+              <button className="btn btn-primary">
+                <i className="fa fa-plus fa-fw" /> Add Lesson
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Dashboard;
