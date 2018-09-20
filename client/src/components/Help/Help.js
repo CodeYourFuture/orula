ae66b3f09fb418fcf163d3728dda9214c60a970f
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-09-20 11:23:48 +0100

    Move help&Home folders to components

---
+++ client/src/components/Help/Help.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class Help extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-lg-12">
+            <h1 className="page-header">Help</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Help;
