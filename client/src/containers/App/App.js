b910bea7b8efabc5184d47a0a5ddd8fc1a5c997a
Shawki Al-Dubaee <shawki@Shawkis-MacBook-Pro.local>
2018-09-29 17:09:36 +0100

    Remove ViewMentorTopics call

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -24,7 +24,6 @@ import AssignUserRole from "../Admin/Users/AssignUserRole";
 import EditUser from "../User/EditUser/EditUser";
 import AssignCourseToStudent from "../Admin/Courses/AssignCourseToStudent/AssignCourseToStudent";
 import ViewStudentTopics from "../User/Topics/ViewStudentTopics";
-import ViewMentorTopics from "../User/Topics/ViewMentorTopics";
 import { getSessionUser, getUserRoles } from "../../helpers/api.js";
 import MentorHome from "../../components/MentorHome/MentorHome";
 
@@ -106,19 +105,6 @@ class App extends Component {
               path="/lesson/:lessonId"
               component={ViewStudentTopics}
             />
-            {this.state.isMentor ? (
-              <Route
-                exact
-                path="/lesson/:lessonId"
-                component={ViewMentorTopics}
-              />
-            ) : (
-              <Route
-                exact
-                path="/lesson/:lessonId"
-                component={ViewStudentTopics}
-              />
-            )}
 
             <Route path="/admin/topics/add" component={AddTopics} />
             <Route path="/admin/topics/edit/:topicId" component={EditTopics} />
