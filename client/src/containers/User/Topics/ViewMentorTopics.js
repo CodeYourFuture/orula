a851436e1a942c2d6acd015e7626ed56a30af04d
Islam Dudaev <islamwolf23@gmail.com>
2018-10-01 09:43:52 +0100

    fix styling of code

--- client/src/containers/User/Topics/ViewMentorTopics.js
+++ client/src/containers/User/Topics/ViewMentorTopics.js
@@ -29,7 +29,7 @@ class ViewMentorTopics extends Component {
     var newRatings = [];
     responses.forEach(res => {
       const { data } = res;
-      newRatings.push(data);
+      newRatings.push(data[0]);
     });
     this.setState({ ratings: newRatings, topics, lessonName: lesson[0].name });
   };
@@ -45,56 +45,59 @@ class ViewMentorTopics extends Component {
         </div>
         <div className="row">
           <div className="col-md-12">
-            {topics.map(topic => (
-              <div key={topic.topic_id} className="table-responsive">
-                <h4>{topic.title}</h4>
-                <table className="table table-striped table-bordered table-hover">
-                  <thead>
-                    <tr>
-                      <th>Student name</th>
-                      <th>Rating before the class</th>
-                      <th>Rating after the class</th>
-                      <th>Rating 3 days later</th>
-                      <th>Rating 1 week later</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {ratings[0].length > 0
-                      ? ratings.map(rating => {
-                          if (topic.topic_id === rating[0].topic_id) {
-                            return (
-                              <tr key={rating[0].rating_id}>
-                                <td>{rating[0].name}</td>
-                                <td>
-                                  {rating[0].rating_before === -1
-                                    ? "Not rated"
-                                    : rating[0].rating_before}
-                                </td>
-                                <td>
-                                  {rating[0].rating_after === -1
-                                    ? "Not rated"
-                                    : rating[0].rating_after}
-                                </td>
-                                <td>
-                                  {rating[0].rating_3days === -1
-                                    ? "Not rated"
-                                    : rating[0].rating_3days}
-                                </td>
-                                <td>
-                                  {rating[0].rating_1week === -1
-                                    ? "Not rated"
-                                    : rating[0].rating_1week}
-                                </td>
-                              </tr>
-                            );
-                          }
-                          return null;
-                        })
-                      : null}
-                  </tbody>
-                </table>
-              </div>
-            ))}
+            {topics.length > 0
+              ? topics.map(topic => (
+                  <div key={topic.topic_id} className="table-responsive">
+                    <h4>{topic.title}</h4>
+                    {ratings.length > 0 ? (
+                      <table className="table table-striped table-bordered table-hover">
+                        <thead>
+                          <tr>
+                            <th>Student name</th>
+                            <th>Rating before the class</th>
+                            <th>Rating after the class</th>
+                            <th>Rating 3 days later</th>
+                            <th>Rating 1 week later</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {ratings.map(rating => {
+                            if (rating === undefined) return null;
+                            if (topic.topic_id === rating.topic_id) {
+                              return (
+                                <tr key={rating.rating_id}>
+                                  <td>{rating.name}</td>
+                                  <td>
+                                    {rating.rating_before === -1
+                                      ? "Not rated"
+                                      : rating.rating_before}
+                                  </td>
+                                  <td>
+                                    {rating.rating_after === -1
+                                      ? "Not rated"
+                                      : rating.rating_after}
+                                  </td>
+                                  <td>
+                                    {rating.rating_3days === -1
+                                      ? "Not rated"
+                                      : rating.rating_3days}
+                                  </td>
+                                  <td>
+                                    {rating.rating_1week === -1
+                                      ? "Not rated"
+                                      : rating.rating_1week}
+                                  </td>
+                                </tr>
+                              );
+                            }
+                            return null;
+                          })}
+                        </tbody>
+                      </table>
+                    ) : null}
+                  </div>
+                ))
+              : null}
             <Link to="/">
               <button className="btn btn-primary">Go back</button>
             </Link>
